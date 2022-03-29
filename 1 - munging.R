f7a637409d2790bd83924a301c964e8a66d68418
Grace Wilkinson <grace.wilkinson23@gmail.com>
2022-03-29 10:58:35 -0500

    adding data file and initializing cleaning script

---
+++ 1 - munging.R
@@ -0,0 +1,16 @@
+#---------------------------
+# Step 1 - Data Munging
+#---------------------------
+# DATA SOURCE: D'Ambrosio and Harrison (2021) L&O Letters
+# Manuscript: https://aslopubs.onlinelibrary.wiley.com/doi/epdf/10.1002/lol2.10192
+# Data Source & Metadata: https://figshare.com/articles/dataset/Methane_production_oxidation_rates_in_lakes_and_reservoirs/12811778
+#---------------------------
+# Script written by G.M. Wilkinson (March 2022) for ZOO 955 as an example of meta-analysis approaches. Analysis intended for demonstration purposes only
+# DO NOT USE FOR STATISTICAL INFERENCE - consult original manuscript and analyses therein for scientific conclusions from this literature synthesis
+#---------------------------
+
+# Packages ===============
+if (!require(tidyverse)) install.packages('tidyverse')
+library(tidyverse)
+
+# Data ===================
